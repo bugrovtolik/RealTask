2da979a7b9f22c3377c42f8b30d76cb2d4e52584
abuhrov <bugrovtolik@gmail.com>
2019-03-30 00:04:30 +0200

    1

--- src/main/java/com/abugrov/helpinghand/config/WebSecurityConfig.java
+++ src/main/java/com/abugrov/helpinghand/config/WebSecurityConfig.java
@@ -23,9 +23,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-//            .csrf()
-//                .ignoringAntMatchers("/task/callback")
-//            .and()
+            .csrf()
+                .ignoringAntMatchers("/task/*/paid")
+            .and()
                 .authorizeRequests().antMatchers(
                     "/",
                     "/registration",

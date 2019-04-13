752720bcb8135581a5a02c4ea6000be1fdbd8fe6
abuhrov <bugrovtolik@gmail.com>
2019-04-13 18:39:43 +0300

    2

--- src/main/java/com/abugrov/helpinghand/config/MvcConfig.java
+++ src/main/java/com/abugrov/helpinghand/config/MvcConfig.java
@@ -1,13 +1,31 @@
 package com.abugrov.helpinghand.config;
 
 import com.cloudinary.Cloudinary;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -19,10 +37,32 @@ public class MvcConfig implements WebMvcConfigurer {
     private String api_key;
     @Value("${cloudinary.api_secret}")
     private String api_secret;
+    @Value("${hostname}")
+    private String host;
 
     @Bean
     public RestTemplate getRestTemplate() {
-        return new RestTemplate();
+        String fixieUrl = System.getenv("FIXIE_URL");
+
+        String[] fixieValues = fixieUrl.split("[/(:\\/@)/]+");
+        String fixieUser = fixieValues[1];
+        String fixiePassword = fixieValues[2];
+        String fixieHost = fixieValues[3];
+        int fixiePort = Integer.parseInt(fixieValues[4]);
+
+        CredentialsProvider credsProvider = new BasicCredentialsProvider();
+        credsProvider.setCredentials(
+                new AuthScope(fixieHost, fixiePort),
+                new UsernamePasswordCredentials(fixieUser, fixiePassword));
+
+        CloseableHttpClient httpclient = HttpClients.custom()
+                .setDefaultCredentialsProvider(credsProvider).build();
+        HttpComponentsClientHttpRequestFactory requestFactory =
+                new HttpComponentsClientHttpRequestFactory();
+
+        requestFactory.setHttpClient(httpclient);
+
+        return new RestTemplate(requestFactory);
     }
 
     @Bean

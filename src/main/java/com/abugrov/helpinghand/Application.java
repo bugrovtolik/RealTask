9916277fdf2f92afc074ac2afac93f98c30ef43a
abuhrov <bugrovtolik@gmail.com>
2019-04-15 22:38:29 +0300

    3

--- src/main/java/com/abugrov/helpinghand/Application.java
+++ src/main/java/com/abugrov/helpinghand/Application.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        System.out.println("before " + System.getProperty("jdk.http.auth.tunneling.disabledSchemes"));
-        System.setProperty("jdk.http.auth.tunneling.disabledSchemes","");
-        System.setProperty("jdk.http.auth.proxying.disabledSchemes","");
-        System.out.println("after " + System.getProperty("jdk.http.auth.tunneling.disabledSchemes"));
+//        System.out.println("before " + System.getProperty("http.auth.tunneling.disabledSchemes"));
+//        System.setProperty("http.auth.tunneling.disabledSchemes","");
+//        System.setProperty("http.auth.proxying.disabledSchemes","");
+//        System.out.println("after " + System.getProperty("http.auth.tunneling.disabledSchemes"));
         SpringApplication.run(Application.class, args);
     }
 }
--- src/main/java/com/abugrov/helpinghand/config/MvcConfig.java
+++ src/main/java/com/abugrov/helpinghand/config/MvcConfig.java
@@ -4,9 +4,17 @@ import com.cloudinary.Cloudinary;
 import okhttp3.Authenticator;
 import okhttp3.Credentials;
 import okhttp3.OkHttpClient;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -32,12 +40,18 @@ public class MvcConfig implements WebMvcConfigurer {
     @Bean
     public RestTemplate getRestTemplate() {
         String fixieUrl = System.getenv("FIXIE_URL");
+        fixieUrl = "http://fixie:cpH8JaRJlBHgvdo@velodrome.usefixie.com:80";
 
         String[] fixieValues = fixieUrl.split("[/(:\\/@)/]+");
         String fixieUser = fixieValues[1];
         String fixiePassword = fixieValues[2];
         String fixieHost = fixieValues[3];
         int fixiePort = Integer.parseInt(fixieValues[4]);
+
+        System.setProperty("http.proxyHost", "hostAddress");
+        System.setProperty("http.proxyPort", "portNumber");
+        System.setProperty("http.proxyUser", fixieUser);
+        System.setProperty("http.proxyPassword", fixiePassword);
 
         OkHttpClient.Builder clientBuilder = new OkHttpClient.Builder();
         Authenticator proxyAuthenticator = (route, response) -> {

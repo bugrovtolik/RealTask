05198846bee9255ebbfe9bc64c8489d9492fd9e1
abuhrov <bugrovtolik@gmail.com>
2019-04-20 15:36:30 +0300

    3

---
+++ Procfile
@@ -0,0 +1,2 @@
+JAVA_OPTS="$JAVA_OPTS -Djdk.http.auth.proxying.disabledSchemes= -Djdk.http.auth.tunneling.disabledSchemes="
+web: java $JAVA_OPTS -jar -Dserver.port=$PORT ./target/RealTask-1.0-SNAPSHOT.jar $JAR_OPTS
--- src/main/java/com/abugrov/helpinghand/Application.java
+++ src/main/java/com/abugrov/helpinghand/Application.java
@@ -7,16 +7,6 @@ import java.io.IOException;
 
 @SpringBootApplication
 public class Application {
-    static {
-        System.out.println("before " + System.getProperty("jdk.http.auth.tunneling.disabledSchemes"));
-        try {
-            Runtime.getRuntime().exec("cmd /c set JAVA_TOOL_OPTIONS=-Djdk.http.auth.tunneling.disabledSchemes=");
-        } catch (IOException e) {
-            System.out.println("EXCEPTION");
-        }
-        System.out.println("after " + System.getProperty("jdk.http.auth.tunneling.disabledSchemes"));
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }

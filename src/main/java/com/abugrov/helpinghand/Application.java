b2e29429ee633f20c5f0cc5dcf892f016134c41f
abuhrov <bugrovtolik@gmail.com>
2019-04-20 15:31:26 +0300

    3

--- src/main/java/com/abugrov/helpinghand/Application.java
+++ src/main/java/com/abugrov/helpinghand/Application.java
@@ -3,11 +3,18 @@ package com.abugrov.helpinghand;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class Application {
     static {
         System.out.println("before " + System.getProperty("jdk.http.auth.tunneling.disabledSchemes"));
-        System.setProperty("jdk.http.auth.tunneling.disabledSchemes", "");
+        try {
+            Runtime.getRuntime().exec("cmd /c set JAVA_TOOL_OPTIONS=-Djdk.http.auth.tunneling.disabledSchemes=");
+        } catch (IOException e) {
+            System.out.println("EXCEPTION");
+        }
+        System.out.println("after " + System.getProperty("jdk.http.auth.tunneling.disabledSchemes"));
     }
 
     public static void main(String[] args) {

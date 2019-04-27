fab1aa4dd30ac0281da3876eaa59f77a9770eef9
abuhrov <bugrovtolik@gmail.com>
2019-04-27 17:07:45 +0300

    3wf

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -jar -Dserver.port=$PORT ./target/RealTask-1.0-SNAPSHOT.jar -Djdk.http.auth.proxying.disabledSchemes= -Djdk.http.auth.tunneling.disabledSchemes=
+web: java -jar -Dserver.port=$PORT ./target/RealTask-1.0-SNAPSHOT.jar
--- src/main/java/com/abugrov/helpinghand/Application.java
+++ src/main/java/com/abugrov/helpinghand/Application.java
@@ -4,12 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
+import java.util.Map;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        System.out.println("JAVA_TOOL_OPTIONS:   " + System.getenv("JAVA_TOOL_OPTIONS"));
+        ProcessBuilder pb = new ProcessBuilder(args);
+        Map<String, String> env = pb.environment();
+        env.put("JAVA_TOOL_OPTIONS", System.getenv("JAVA_TOOL_OPTIONS") + " -Djdk.http.auth.proxying.disabledSchemes= -Djdk.http.auth.tunneling.disabledSchemes=");
+        Process process = pb.start();
         System.out.println("JAVA_TOOL_OPTIONS:   " + System.getenv("JAVA_TOOL_OPTIONS"));
 
         SpringApplication.run(Application.class, args);
+        System.out.println("DESTROYEDD");
+        process.destroy();
     }
 }

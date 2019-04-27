2e7f20715a88ea4a3a84406639a1300fea48cf07
abuhrov <bugrovtolik@gmail.com>
2019-04-27 17:23:18 +0300

    r

--- src/main/java/com/abugrov/helpinghand/Application.java
+++ src/main/java/com/abugrov/helpinghand/Application.java
@@ -4,20 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.Map;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) throws IOException {
-        System.out.println("JAVA_TOOL_OPTIONS:   " + System.getenv("JAVA_TOOL_OPTIONS"));
-        ProcessBuilder pb = new ProcessBuilder(args);
-        Map<String, String> env = pb.environment();
-        env.put("JAVA_TOOL_OPTIONS", System.getenv("JAVA_TOOL_OPTIONS") + " -Djdk.http.auth.proxying.disabledSchemes= -Djdk.http.auth.tunneling.disabledSchemes=");
-        Process process = pb.start();
-        System.out.println("JAVA_TOOL_OPTIONS:   " + System.getenv("JAVA_TOOL_OPTIONS"));
-
+    public static void main(String[] args) {
+        try {
+            Map<String, String> env = System.getenv();
+            Class<?> cl = env.getClass();
+            Field field = cl.getDeclaredField("JAVA_TOOL_OPTIONS");
+            field.setAccessible(true);
+            Map<String, String> writableEnv = (Map<String, String>) field.get(env);
+            writableEnv.put("JAVA_TOOL_OPTIONS", System.getenv("JAVA_TOOL_OPTIONS") + " -Djdk.http.auth.proxying.disabledSchemes= -Djdk.http.auth.tunneling.disabledSchemes=");
+        } catch (Exception e) {
+            throw new IllegalStateException("Failed to set environment variable", e);
+        }
+        System.out.println("JAVA_TOOL_OPTIONS: " + System.getenv("JAVA_TOOL_OPTIONS"));
         SpringApplication.run(Application.class, args);
-        System.out.println("DESTROYEDD");
-        process.destroy();
     }
 }
